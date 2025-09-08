f1822af2a68e53b3b40fc256e2b4137729502ada
atsuchak <suchak0630@gmail.com>
2025-09-08 19:20:15 +0600

    init git

---
+++ BootCamp Practice/Practice 2/1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int a, b, c; cin >> a >> b >> c;
+
+	cout << a*b*c;
+
+	return 0;
+}

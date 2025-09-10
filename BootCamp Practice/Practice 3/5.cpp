edda279ae7ccb8f920418f1d4669d2c1bad6e257
atsuchak <suchak0630@gmail.com>
2025-09-10 19:09:06 +0600

    solve problem 1-5

---
+++ BootCamp Practice/Practice 3/5.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+
+	for (int i = 2; i <= n; i += 2) cout << i << endl;
+
+	return 0;
+}

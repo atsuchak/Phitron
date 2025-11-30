850ed7eb57dfb97c21e6a7a6bed51c0ff8f6116e
atsuchak <suchak0630@gmail.com>
2025-11-30 20:21:19 +0600

    create string_constructor file

---
+++ C++ for DSA/Module 6/string_constructor.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("D:/File/input.txt", "r", stdin);
+    freopen("D:/File/output.txt", "w", stdout);
+#endif
+
+    cout << "Hello world" << endl;
+
+    return 0;
+}

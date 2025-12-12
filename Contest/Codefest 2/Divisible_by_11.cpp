88d41caea3db9ff5755f352a91ace0b921046d00
atsuchak <suchak0630@gmail.com>
2025-12-13 00:24:37 +0600

    complete codefest 2

---
+++ Contest/Codefest 2/Divisible_by_11.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = "; _print(x); cerr << endl; 
+#else 
+    #define debug(x)
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void _print(int n) {cerr << n;}
+void _print(ll n) {cerr << n;}
+void _print(double n) {cerr << n;}
+void _print(char n) {cerr << n;}
+void _print(string n) {cerr << n;}
+void _print(bool n) {cerr << n;}
+
+template<class T, class V> void _print(pair<T, V> p) {
+    cerr << "("; _print(p.ff); cerr << ", "; _print(p.ss); cerr << ")";};
+template<class T> void _print(vector<T> v) {
+    cerr << "[ "; for(T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template<class T> void _print(set<T> s) {
+    cerr << "[ "; for(T i : s) {_print(i); cerr << " ";} cerr << "]";}
+template<class T, class V> void _print(map<T, V> m) {
+    cerr << "[ "; for(auto i : m) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve() {
+    string st; cin >> st;
+    
+    int oSum = 0, eSum = 0;
+    for(int i = sz(st) - 1; i >= 0; i--) {
+        int x = st[i] - '0';
+        int idx = sz(st) - i; 
+        
+        if(idx % 2 == 1) oSum += x;
+        else eSum += x;
+    }
+    
+    int diff = abs(oSum - eSum);
+    
+    if(diff % 11 == 0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        // freopen("D:/File/input.txt", "r", stdin);
+        // freopen("D:/File/output.txt", "w", stdout);
+        freopen("D:/File/error.txt", "w", stderr);
+    #endif
+
+    int t_case = 1;
+    // cin >> t_case;
+
+    while (t_case--) {
+        solve();
+    }
+
+    return 0;
+} 
---
+++ Contest/Codefest 2/Treasure_in_the_Grid.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = "; _print(x); cerr << endl; 
+#else 
+    #define debug(x)
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void _print(int n) {cerr << n;}
+void _print(ll n) {cerr << n;}
+void _print(double n) {cerr << n;}
+void _print(char n) {cerr << n;}
+void _print(string n) {cerr << n;}
+void _print(bool n) {cerr << n;}
+
+template<class T, class V> void _print(pair<T, V> p) {
+    cerr << "("; _print(p.ff); cerr << ", "; _print(p.ss); cerr << ")";};
+template<class T> void _print(vector<T> v) {
+    cerr << "[ "; for(T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template<class T> void _print(set<T> s) {
+    cerr << "[ "; for(T i : s) {_print(i); cerr << " ";} cerr << "]";}
+template<class T, class V> void _print(map<T, V> m) {
+    cerr << "[ "; for(auto i : m) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve() {
+    int n, m, q; cin >> n >> m >> q;
+    
+    int grid[100][100];;
+    for(int i = 0; i < n; i++) 
+        for(int j = 0; j < m; j++) cin >> grid[i][j];
+    
+    
+    for(int k = 0; k < q; k++) {
+        int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+        
+        int sum = 0;
+        for(int i = x1; i <= x2; i++) 
+            for(int j = y1; j <= y2; j++) sum += grid[i][j];
+        
+        cout << sum << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        // freopen("D:/File/input.txt", "r", stdin);
+        // freopen("D:/File/output.txt", "w", stdout);
+        freopen("D:/File/error.txt", "w", stderr);
+    #endif
+
+    int t_case = 1;
+    // cin >> t_case;
+
+    while (t_case--) {
+        solve();
+    }
+
+    return 0;
+} 

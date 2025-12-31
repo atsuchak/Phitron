33931bb75ca9ec8160252d931cd8c24713c9f10c
atsuchak <suchak0630@gmail.com>
2026-01-01 01:12:22 +0600

    delete a node from any position of a linked list

---
+++ Basic Data Structure/Module 6/delete_at_any_pos.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    
+    public: 
+        int val;
+        Node* next;
+
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val) {
+    Node* newNode = new Node(val);
+
+    if(head == NULL) {
+        head = newNode;
+        tail = newNode;
+    }else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+void delete_at_pos(Node* &head, int idx) {
+    Node* temp = head;
+    
+    for(int i = 1; i < idx; i++) temp = temp->next;
+
+    Node* deleteNode = temp->next;
+    temp->next = temp->next->next;
+
+    delete deleteNode;
+}
+
+void print_ll(Node* &tmp) {
+    if(tmp == NULL) return;
+
+    cout << tmp->val << " ";
+    print_ll(tmp->next);
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("D:/File/input.txt", "r", stdin);
+    freopen("D:/File/output.txt", "w", stdout);
+#endif
+
+    Node* head = NULL;
+    Node* tail = NULL;
+    
+    int val;
+    while(1) {
+        cin >> val;
+        
+        if(val == -1) break;
+
+        insert_at_tail(head, tail, val);
+    }
+
+    cout << "Before delete: ";
+    print_ll(head);
+    cout << endl;
+    
+    delete_at_pos(head, 2);
+
+    cout << "After delete: ";
+    print_ll(head);
+
+    return 0;
+}

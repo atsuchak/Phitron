9bdb2f20a26a4f05af8e1525c75c97ef563914b5
atsuchak <suchak0630@gmail.com>
2026-01-20 20:47:14 +0600

    participated in problem solving contest 3

---
+++ Contest/Problem Solving-3 (Phitron mentorship)/C_Chessboard.cpp
@@ -0,0 +1,102 @@
+// --------------------------------------------------------------- //
+// --------------------------------------------------------------- //
+// -- If it works, don't touch it                              --- //
+// -- Dedicated to the test case I forgot to consider          --- //
+// -- Logic is a systematic way of going wrong with confidence --- //
+// --------------------------------------------------------------- //
+// --------------------------------------------------------------- //
+
+//Problem link: https://vjudge.net/contest/781854#problem/C
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = "; _print(x); cerr << endl; 
+#else 
+    #define debug(x)
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void _print(int n) {cerr << n;}
+void _print(ll n) {cerr << n;}
+void _print(double n) {cerr << n;}
+void _print(char n) {cerr << n;}
+void _print(string n) {cerr << n;}
+void _print(bool n) {cerr << n;}
+
+template<class T, class V> void _print(pair<T, V> p) {
+    cerr << "("; _print(p.ff); cerr << ", "; _print(p.ss); cerr << ")";};
+template<class T> void _print(vector<T> v) {
+    cerr << "[ "; for(T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template<class T> void _print(set<T> s) {
+    cerr << "[ "; for(T i : s) {_print(i); cerr << " ";} cerr << "]";}
+template<class T, class V> void _print(map<T, V> m) {
+    cerr << "[ "; for(auto i : m) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve() {
+
+    char arr[8][8];
+    int r, c;
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
+            cin >> arr[i][j];
+
+            if(arr[i][j] == '*') {
+                r = 8-i;
+                c = j;
+                break;
+            }
+        }
+    }
+
+    if(c == 0) cout << 'a' << r << endl;
+    else if(c == 1) cout << 'b' << r << endl;
+    else if(c == 2) cout << 'c' << r << endl;
+    else if(c == 3) cout << 'd' << r << endl;
+    else if(c == 4) cout << 'e' << r << endl;
+    else if(c == 5) cout << 'f' << r << endl;
+    else if(c == 6) cout << 'g' << r << endl;
+    else if(c == 7) cout << 'h' << r << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("D:/File/input.txt", "r", stdin);
+        freopen("D:/File/output.txt", "w", stdout);
+        freopen("D:/File/error.txt", "w", stderr);
+    #endif
+
+    int t_case = 1;
+    // cin >> t_case;
+
+    while (t_case--) {
+        solve();
+    }
+
+    return 0;
+} 
